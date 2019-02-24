e081ffe95de50c1a60b3faa64d04b0cfc400f09e
Everton Pires <epe.masork@gmail.com>
2019-02-24 18:53:20 -0300

    Main Viwe Designer

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,8 +13,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); //instanciação
-			Parent parent = loader.load();//carregar a View
-			Scene mainScene = new Scene(parent);//cena principal passando como argumento a linha View
+			ScrollPane scrollPane = loader.load();//carregar a View
+			scrollPane.setFitToHeight(true); // ajustando a barra de menu, para ficar até o final da altura
+			scrollPane.setFitToWidth(true);// ajustando a barra de menu, para ficar até o final da largura
+//---------------------------------------------------------------------------------------------------------------------------------------------------------				
+			
+			Scene mainScene = new Scene(scrollPane);//cena principal passando como argumento a linha View
 			primaryStage.setScene(mainScene);//palco com a cena principal
 			primaryStage.setTitle("Aplicação JavaFX de Modelo");//Titulo para o Palco
 			primaryStage.show();//mostrar o palco
