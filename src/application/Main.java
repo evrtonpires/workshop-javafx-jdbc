190d581e58eb3fd4be6faebd260dca9cc8ee768b
Everton Pires <epe.masork@gmail.com>
2019-02-24 20:01:07 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,8 +9,13 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	//referencia para a CENA	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
+				
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); //instanciação
 			ScrollPane scrollPane = loader.load();//carregar a View
@@ -18,15 +23,21 @@ public class Main extends Application {
 			scrollPane.setFitToWidth(true);// ajustando a barra de menu, para ficar até o final da largura
 //---------------------------------------------------------------------------------------------------------------------------------------------------------				
 			
-			Scene mainScene = new Scene(scrollPane);//cena principal passando como argumento a linha View
+			mainScene = new Scene(scrollPane);//cena principal passando como argumento a linha View
 			primaryStage.setScene(mainScene);//palco com a cena principal
 			primaryStage.setTitle("Aplicação JavaFX de Modelo");//Titulo para o Palco
 			primaryStage.show();//mostrar o palco
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+//---------------------------------------------------------------------------------------------------------------------------------------------------------					
+		
+	//referencia para a CENA	
 	}
-
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+//---------------------------------------------------------------------------------------------------------------------------------------------------------			
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 	//---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -19,6 +28,9 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	private MenuItem menuItemAbout;
+	
+	
+	
 	//---------------------------------------------------------------------------------------------------------------------------------------------------------
 																		//METODOS DE CONTROLES DE TELA
 	//---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -34,9 +46,8 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+	loadView("/gui/About.fxml");
 	}
-	
 	//---------------------------------------------------------------------------------------------------------------------------------------------------------
 																		//METODOS DA INTERFACE " Initializable "
 	//---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -44,6 +55,37 @@ public class MainViewController implements Initializable{
 	public void initialize(URL uri, ResourceBundle rb) {
 		
 		
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------------------------------
+																		//METODOS DA INTERFACE SECUNDARIA, CRIANDO OUTRA TELA.
+	//---------------------------------------------------------------------------------------------------------------------------------------------------------
+	private synchronized void loadView(String absoluteName) {// synchronized -  garantir que todo o processo seja interrompido durante o multtreding
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));//instanciação
+			
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();//referencia CENA
+		
+			VBox mainVBox =(VBox)//Casting para referencia do VBOX Principal = Classe MainViewController
+				((ScrollPane) //Casting para referencia do ScrollPane = Classe MainView
+				mainScene.getRoot())//pega o primeiro elemento da View = ScrollPane
+				.getContent();//referencia para o que tiver dentro do ScrollPane
+		
+		
+		//refencia para o menu
+		
+		Node mainMenu = mainVBox.getChildren().get(0);// pegando o primeiro filho do VBOX da janela principal
+		
+		mainVBox.getChildren().clear();// limpar todos os filhos do mainVBOX
+		
+		mainVBox.getChildren().add(mainMenu);// adicionando o Menu
+		mainVBox.getChildren().addAll(newVBox.getChildren());//adicionando os filhos do NewVBOX
+		
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Erro Carregando a Pagina", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
