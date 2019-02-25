e7560b35dfca5a854aa4bb8f8104101569da62a5
Everton Pires <epe.masork@gmail.com>
2019-02-24 23:22:03 -0300

    DepartmentFormController

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,58 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable{
+	
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+																			//ITENS DE CONTROLES DE TELA
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtNome;
+	
+	@FXML
+	private Label labelErroNome;
+	
+	@FXML
+	private Button btSalvar;
+	
+	@FXML
+	private Button btCancelar;
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+																			//METODOS DE CONTROLES DE TELA
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+	@FXML
+	public void onBtSalvarAction() {
+		System.out.println("onBtSalvarAction");
+	}
+	
+	@FXML
+	public void onBtCancelarAction() {
+		System.out.println("onBtCancelarAction");
+	}
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);// ide só aceita numero inteiro
+		Constraints.setTextFieldMaxLength(txtNome, 30); // nome tera no maximo 30 caracteres 
+	}
+	
+
+}
